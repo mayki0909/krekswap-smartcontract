46eb8c17d4e8c2b2ce71795bdfbb9af4bffb0cea
Miha Žnidar <mayki.znidar@gmail.com>
2023-07-09 13:28:56 +0200

    Permit test

--- test/Token.ts
+++ test/Token.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Signer, parseEther } from "ethers";
+import { Signer, parseEther, verifyTypedData } from "ethers";
 import { Token } from "../typechain-types";
 
 describe('Token', function () {
@@ -20,8 +20,6 @@ describe('Token', function () {
     const Token = await ethers.getContractFactory("Token");
     token = await Token.deploy(name, symbol, initialSupply) as Token;
     await token.waitForDeployment()
-
-    // console.log("Token deployed to:", await token.getAddress());
   });
 
   it("should have correct name, symbol, and initial supply", async function () {
@@ -49,11 +47,76 @@ describe('Token', function () {
   it("should burn tokens", async function () {
     const initialBalance = await token.balanceOf(await owner.getAddress());
     const amountToBurn = parseEther("100");
-    initialBalance
 
     await token.connect(owner).burn(amountToBurn);
 
     const finalBalance = await token.balanceOf(await owner.getAddress());
     expect(finalBalance).to.equal(initialBalance - amountToBurn);
   });
+
+  it("permit", async function () {
+    const address1 = await addr1.getAddress()
+    const address2 = await addr2.getAddress()
+
+    const eip712 = await token.eip712Domain()
+    //0: bytes1: fields 0x0f
+    //1: string: name token
+    //2: string: version 1
+    //3: uint256: chainId 1
+    //4: address: verifyingContract 0xd9145CCE52D386f254917e481eB44e9943F39138
+    //5: bytes32: salt 0x0000000000000000000000000000000000000000000000000000000000000000
+    //6:  uint256[]: extensions
+    
+    const name = eip712[0]
+    const version = eip712[2]
+    const chainId = eip712[3]
+    const verifyingContract = eip712[4]
+    const nonce = await token.nonces(address1)
+    const deadline = '2000000000'
+    const value = 100
+
+    const domain = { name, version, chainId, verifyingContract }
+    const types = {
+      Permit: [{
+          name: "owner",
+          type: "address"
+        },
+        {
+          name: "spender",
+          type: "address"
+        },
+        {
+          name: "value",
+          type: "uint256"
+        },
+        {
+          name: "nonce",
+          type: "uint256"
+        },
+        {
+          name: "deadline",
+          type: "uint256"
+        },
+      ],
+    }
+    const values = { owner: address1, spender: address2, value, nonce, deadline }
+
+    const signature = await addr1.signTypedData(domain, types, values)
+    const recovered = await verifyTypedData(domain, types, values, signature)
+
+    expect(recovered).to.equal(address1)
+  
+    // DEPRECATED IN ETHERS V6
+    // const { v, r, s } = ethers.utils.splitSignature(signature)
+
+    // Getting an ERROR
+    // const signatureBuffer = Buffer.from(signature, 'hex');
+    // const r = signatureBuffer.slice(0, 32).toString('hex');
+    // const s = signatureBuffer.slice(32, 64).toString('hex');
+    // const v = signatureBuffer[64].toString(10);
+    
+    // await token.connect(addr1).permit(address1, address2, value, deadline, v, r, s)
+
+  })
 })
+
