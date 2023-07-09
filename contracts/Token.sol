fbb24ccbfa7662300ade613f2516edb494357106
Miha Žnidar <mayki.znidar@gmail.com>
2023-07-09 12:41:45 +0200

    Added permit to token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Token is ERC20, ERC20Burnable, Ownable{
+contract Token is Ownable, ERC20, ERC20Burnable, ERC20Permit {
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _initialSupply
-    ) ERC20(_name, _symbol) {
+    ) ERC20(_name, _symbol) ERC20Permit(_name) {
         _mint(msg.sender, _initialSupply);
     }
 
